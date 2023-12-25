e9ef4f864750f2592dd442d840973bff29bd652f
johnsamey <johnsamyut@gmail.com>
2023-12-25 02:23:42 +0200

    modify simple order

--- src/main/java/com/APIX/order/model/SimpleOrder.java
+++ src/main/java/com/APIX/order/model/SimpleOrder.java
@@ -1,4 +1,28 @@
 package com.APIX.order.model;
 
-public class SimpleOrder {
+import com.APIX.Product.model.Product;
+
+import java.util.List;
+
+public class SimpleOrder extends Order{
+    public SimpleOrder(int id, List<Product> products, double shippingFee) {
+        super(id, products, shippingFee);
+    }
+    public double getTotalPrice() {
+        double totalPrice = 0;
+        for (Product product : getProducts()) {
+            totalPrice += product.getPrice();
+        }
+        totalPrice += getShippingFee();
+        return totalPrice;
+    }
+    public void printDetails() {
+        System.out.println("This order has id " + getId() + " and status " + getStatus() + ".");
+        System.out.println("This order contains " + getProducts().size() + " products.");
+        for (Product product : getProducts()) {
+            System.out.println(product.getName() + ": " + product.getPrice());
+        }
+        System.out.println("The shipping fee of this order is " + getShippingFee() + ".");
+        System.out.println("The total price of this order is " + getTotalPrice() + ".");
+    }
 }
